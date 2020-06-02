511037499b413bb686b26cf7e344db649f9bd113
miaomiaomiao66 <56527607+miaomiaomiao66@users.noreply.github.com>
2020-06-02 09:32:04 +0800

    debug
    debug

--- js/analysis.js
+++ js/analysis.js
@@ -18,10 +18,9 @@ $.ajax({
            typ[i]=data[i].type;
            number[i]=data[i].number;
        }
-    //    alert(number[5]);
+        //alert(number[5]);
     }
 })
-
 var day1=[]
 var country=[]
 var num=[]
@@ -162,7 +161,7 @@ for(i=0;i<date.length;i++){
         inf++;
     }
 }
-// alert(tdnumber[inf-1]);
+//alert(tdnumber[inf-1]);
 option = {
 title: {
     text: 'GDELT单日事件态度统计',
@@ -184,13 +183,7 @@ series: [
         type: 'pie',
         radius: '55%',
         center: ['50%', '60%'],
-        data: [
-            {value: 335, name: '直接访问'},
-            {value: 310, name: '邮件营销'},
-            {value: 234, name: '联盟广告'},
-            {value: 135, name: '视频广告'},
-            {value: 1548, name: '搜索引擎'}
-        ],
+        data: [],
         emphasis: {
             itemStyle: {
                 shadowBlur: 10,
